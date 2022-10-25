3427d9c8e5175737df8dfae2343ff72f010adb48
cbspace <24204459+cbspace@users.noreply.github.com>
2022-10-25 20:59:10 +1000

    SSI syntax updated and build script added

---
+++ package_extension.sh
@@ -0,0 +1,27 @@
+# Script to build the VSIX package
+if [ $# -eq 0 ]
+then
+    echo "Usage: ./package_extension.sh [microlokii|ssi]"
+    exit 1
+fi
+
+if [ -d "$1" ]
+then
+    cd $1
+    if [ -d "extension" ]
+    then
+        rm -r extension/*
+    else
+        mkdir extension
+    fi
+    cp language-configuration.json extension/
+    cp LICENSE.md extension/
+    cp package.json extension/
+    cp -r syntaxes extension/
+
+    cd extension
+    vsce package
+    mv *.vsix ../../packaged
+else
+    echo "Error: Directory \"$1\" does not exist"
+fi
